895e404fbad5f2768e61a236b7a3049fc3870484
Farhad-Rezwan <farhad.rezwan@gmail.com>
2022-08-07 00:59:16 +1000

    Resolve lualine

--- .config/nvim/lua/lualine-config/init.lua
+++ .config/nvim/lua/lualine-config/init.lua
@@ -1,4 +1,5 @@
 require('lualine').setup({
   options = {theme = 'rose-pine', section_separators = '', component_separators = ''},
-  extensions = {'nvim-tree'}
+  extensions = {'nvim-tree'},
+  disabled_filetypes = {'nvim-tree'}
 })
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -11,7 +11,7 @@ return require('packer').startup({
   function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-    use { 'rose-pine/neovim', config = "vim.cmd('colorscheme rose-pine')" }
+    use {'rose-pine/neovim', config = "vim.cmd('colorscheme rose-pine')"}
     use {
       'nvim-treesitter/nvim-treesitter',
       run = ":TSUpdate",
@@ -20,7 +20,7 @@ return require('packer').startup({
     }
     use {
       'tamton-aquib/staline.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+      requires = {'kyazdani42/nvim-web-devicons', opt = true},
       event = "BufRead",
       config = "require('staline-config')"
     }
@@ -36,50 +36,50 @@ return require('packer').startup({
       cmd = "NvimTreeToggle",
       config = "require('nvim-tree-config')"
     }
-    use { 'windwp/nvim-ts-autotag', event = "InsertEnter", after = "nvim-treesitter" }
-    use { 'p00f/nvim-ts-rainbow', after = "nvim-treesitter" }
-    use { 'windwp/nvim-autopairs', config = "require('autopairs-config')", after = "nvim-cmp" }
-    use { 'folke/which-key.nvim', event = "BufWinEnter", config = "require('whichkey-config')" }
+    use {'windwp/nvim-ts-autotag', event = "InsertEnter", after = "nvim-treesitter"}
+    use {'p00f/nvim-ts-rainbow', after = "nvim-treesitter"}
+    use {'windwp/nvim-autopairs', config = "require('autopairs-config')", after = "nvim-cmp"}
+    use {'folke/which-key.nvim', event = "BufWinEnter", config = "require('whichkey-config')"}
     use {
       'nvim-telescope/telescope.nvim',
-      requires = { { 'nvim-lua/plenary.nvim' } },
+      requires = {{'nvim-lua/plenary.nvim'}},
       cmd = "Telescope",
       config = "require('telescope-config')"
     }
-    use { 'neovim/nvim-lspconfig', config = "require('lsp')" }
-    use { 'hrsh7th/cmp-nvim-lsp' }
-    use { 'hrsh7th/cmp-buffer' }
-    use { 'hrsh7th/nvim-cmp' }
-    use { 'hrsh7th/cmp-vsnip' }
-    use { 'hrsh7th/vim-vsnip' }
-    use { 'onsails/lspkind-nvim' }
-    use { 'norcalli/nvim-colorizer.lua', config = "require('colorizer-config')", event = "BufRead" }
+    use {'neovim/nvim-lspconfig', config = "require('lsp')"}
+    use {'hrsh7th/cmp-nvim-lsp'}
+    use {'hrsh7th/cmp-buffer'}
+    use {'hrsh7th/nvim-cmp'}
+    use {'hrsh7th/cmp-vsnip'}
+    use {'hrsh7th/vim-vsnip'}
+    use {'onsails/lspkind-nvim'}
+    use {'norcalli/nvim-colorizer.lua', config = "require('colorizer-config')", event = "BufRead"}
     use {
       'lewis6991/gitsigns.nvim',
-      requires = { 'nvim-lua/plenary.nvim' },
+      requires = {'nvim-lua/plenary.nvim'},
       config = function()
-        require('gitsigns').setup { current_line_blame = true }
+        require('gitsigns').setup {current_line_blame = true}
       end
     }
-    use { 'glepnir/dashboard-nvim', event = "BufRead", config = "require('dashboard-config')" }
+    use {'glepnir/dashboard-nvim', event = "BufRead", config = "require('dashboard-config')"}
     use {
       "lukas-reineke/indent-blankline.nvim",
       config = "require('blankline-config')",
       event = "BufRead"
     }
-    use { "akinsho/toggleterm.nvim", config = "require('toggleterm-config')" }
-    use { "terrortylor/nvim-comment", config = "require('comment-config')" }
-    use { 'tami5/lspsaga.nvim', config = "require('lspsaga-config')" }
-    use { 'williamboman/nvim-lsp-installer' }
-    use { 'jose-elias-alvarez/null-ls.nvim', config = "require('null-ls-config')" }
-    use { "folke/zen-mode.nvim", config = 'require("zen-mode-config")' }
-    use { "folke/twilight.nvim", config = "require('twilight-config')" }
-    use { 'andweeb/presence.nvim', config = "require('presence-config')" }
+    use {"akinsho/toggleterm.nvim", config = "require('toggleterm-config')"}
+    use {"terrortylor/nvim-comment", config = "require('comment-config')"}
+    use {'tami5/lspsaga.nvim', config = "require('lspsaga-config')"}
+    use {'williamboman/nvim-lsp-installer'}
+    use {'jose-elias-alvarez/null-ls.nvim', config = "require('null-ls-config')"}
+    use {"folke/zen-mode.nvim", config = 'require("zen-mode-config")'}
+    use {"folke/twilight.nvim", config = "require('twilight-config')"}
+    use {'andweeb/presence.nvim', config = "require('presence-config')"}
   end,
   config = {
     display = {
       open_fn = function()
-        return require('packer.util').float({ border = 'single' })
+        return require('packer.util').float({border = 'single'})
       end
     }
   }
